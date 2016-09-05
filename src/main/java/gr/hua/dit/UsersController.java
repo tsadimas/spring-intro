7996d4025e4a09500715712c4708394aea90ab7a
anargyros_tsadimas <tsadimas@gmail.com>
2016-09-05 15:57:31 +0300

    fixes in admin

--- src/main/java/gr/hua/dit/UsersController.java
+++ src/main/java/gr/hua/dit/UsersController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
